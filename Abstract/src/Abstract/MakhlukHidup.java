39758069fb448744fcdf92d66022c872a84f5ddc
yoihenken <18104005@ittelkom-pwt.ac.id>
2019-12-25 22:54:25 +0700

    class yang menunjukkan behavior Makhluk Hidup

---
+++ Abstract/src/Abstract/MakhlukHidup.java
@@ -0,0 +1,14 @@
+package Abstract;
+
+import javax.swing.plaf.PanelUI;
+
+abstract class MakhlukHidup {
+    public void berdiri(){
+    }
+    public void oksigen(){
+        System.out.println("- butuh Makanan");
+        System.out.println("- butuh Oksigen");
+        System.out.println("- butuh Air");
+    }
+
+}
